eb374e2a77989cfc5ce2aefae345c1bb7f36ffb7
Piyushmini <101254031+Piyushmini@users.noreply.github.com>
2022-03-09 15:37:47 +0530

    Add files via upload

---
+++ code.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class GFG{
+	static public void Main (){
+		int a;
+	    a=Convert.ToInt32(Console.ReadLine());
+	   
+		if(a%2==0){
+		    Console.WriteLine("even");
+		}
+		else{
+		    Console.WriteLine("odd");
+		}
+	}
+}
