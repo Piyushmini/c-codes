59320c9a3b9e9c6287b310925396e7061a608c43
Piyushmini <101254031+Piyushmini@users.noreply.github.com>
2022-03-09 15:51:59 +0530

    Add files via upload

---
+++ greater.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class GFG{
+	static public void Main (){
+		int a,b;
+	    a=Convert.ToInt32(Console.ReadLine());
+	    b=Convert.ToInt32(Console.ReadLine());
+		if(a>b){
+		    Console.WriteLine("a is greater");
+		}
+		else{
+		    Console.WriteLine("b is greater");
+		}
+	}
+}
