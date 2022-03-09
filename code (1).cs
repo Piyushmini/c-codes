888642565e9f78b3761c4f25d7abb9e47eb9dd51
Piyushmini <101254031+Piyushmini@users.noreply.github.com>
2022-03-09 15:50:00 +0530

    Add files via upload

---
+++ code (1).cs
@@ -0,0 +1,10 @@
+using System;
+
+public class GFG{
+	static public void Main (){
+		int a,b;
+	    a=Convert.ToInt32(Console.ReadLine());
+	    b=Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine(+(a+b)/2);
+	}
+}
